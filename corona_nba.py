babf9677ee77c3b9721e63e4cef101ba2948219c
Alex Kazorian <akazorian@berkeley.edu>
2020-03-11 23:19:39 -0700

    Working

--- corona_nba.py
+++ corona_nba.py
@@ -5,30 +5,36 @@ import datetime
 
 GAMEIDIDX = 0
 GAMEDATEIDX = 1
+GAMETIMEIDX = 2
 AWAYFULLTEAMNAMEIDX = 5
 HOMEFULLTEAMNAMEIDX = 6
 
-median_day_of_infection = datetime.datetime.strptime('3/6/20', '%m/%d/%y')
+contagious_time = datetime.timedelta(days=14)
 date_of_announcement = datetime.datetime.strptime('3/11/20', '%m/%d/%y')
+date_of_infection = date_of_announcement - contagious_time
 infected_team = 'Utah Jazz'
 
 csv_file_name = "nba-full-schedule-2019-2020.csv"
 with open(csv_file_name, 'r') as csv_file:
 	csv_reader = csv.reader(csv_file)
 	teams = dict()
-	root = None
+	canceled_game = None
 	next(csv_reader)
 	for line in csv_reader:
 		game_id = line[GAMEIDIDX]
+		game_time = line[GAMETIMEIDX]
 
 		game_date = datetime.datetime.strptime(line[GAMEDATEIDX], '%m/%d/%y')
-		if game_date > date_of_announcement:
-			break
+		game_time = datetime.datetime.strptime("{} {}".format(line[GAMEDATEIDX], game_time), '%m/%d/%y  %I:%M %p')
 
 		home_team = line[HOMEFULLTEAMNAMEIDX]
 		away_team = line[AWAYFULLTEAMNAMEIDX]
 
-		game = {"id": game_id, "date": game_date, "away_team": away_team, "home_team": home_team}
+		game = {"id": game_id, "game_time": game_time, "date": game_date, "away_team": away_team, "home_team": home_team}
+
+		if game_date == date_of_announcement and (infected_team == game["away_team"] or infected_team == game["home_team"]):
+			canceled_game = game
+			break
 
 		if home_team not in teams:
 			teams[home_team] = list()
@@ -38,15 +44,13 @@ with open(csv_file_name, 'r') as csv_file:
 			teams[away_team] = list()
 		teams[away_team].append(game)
 
-		if game_date == date_of_announcement and (infected_team == game["away_team"] or infected_team == game["home_team"]):
-			root = game
-
+	cancellation_time = canceled_game['game_time']
 	infected_teams = set()
 	infected_teams.add(infected_team)
 	visited_games = set()
 	stack_of_games = list()
 	for root_game in reversed(teams[infected_team]):
-		if root_game["date"] >= median_day_of_infection:
+		if date_of_infection < root_game["date"] and root_game["date"] < cancellation_time:
 			stack_of_games.append(root_game)
 	 
 	while len(stack_of_games) > 0:
@@ -61,12 +65,12 @@ with open(csv_file_name, 'r') as csv_file:
 			infected_teams.add(game['away_team'])
 
 		for home_team_game in reversed(teams[game['home_team']]):
-			if home_team_game['date'] < median_day_of_infection:
+			if date_of_infection < home_team_game['date'] and home_team_game['game_time'] < cancellation_time:
 				break
 			stack_of_games.append(home_team_game)
 
 		for away_team_game in reversed(teams[game['away_team']]):
-			if away_team_game['date'] < median_day_of_infection:
+			if date_of_infection < away_team_game['date'] and away_team_game['game_time'] < cancellation_time:
 				break
 			stack_of_games.append(away_team_game)
 
@@ -74,20 +78,3 @@ with open(csv_file_name, 'r') as csv_file:
 	print("Infected teams: ")
 	for team in infected_teams:
 		print("\t- ", team)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
